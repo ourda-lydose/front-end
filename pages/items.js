651262063501232c7dc67a2e9a324e07c39f4792
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2024-05-08 20:52:53 +0700

    attempt to add an items.js file with async

---
+++ pages/items.js
@@ -0,0 +1,43 @@
+// pages/items.js
+
+import React from 'react';
+
+const ItemsPage = ({ items }) => {
+    return (
+        <div>
+            <h1>Items List</h1>
+            <table>
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+                </thead>
+                <tbody>
+                {items.map(item => (
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.name}</td>
+                        <td>{item.price}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export async function getStaticProps() {
+    // Fetch item data from an API or database asynchronously
+    const res = await fetch('https://lalala/items');
+    const items = await res.json();
+
+    return {
+        props: {
+            items,
+        },
+    };
+}
+
+export default ItemsPage;
